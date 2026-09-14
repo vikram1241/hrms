ea1da2637dcfa048c4b038e4f390f1de91ab1ff2
vignesh-potharaj <freelancer.nenu@gmail.com>
2026-09-14 20:52:00 +0530

    fix(cf): correct import statement for useEffect in CFIssuePanel

--- client/src/features/documents/CFIssuePanel.jsx
+++ client/src/features/documents/CFIssuePanel.jsx
@@ -1,4 +1,4 @@
-import { useMemo, useState } from 'react';
+import { UuseEffect, useMemo, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import TextField from '@mui/material/TextField';
 import MenuItem from '@mui/material/MenuItem';
